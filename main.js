29f412b859c173e7e4c36627b0768c3817ca1502
misaelpaulmamangun <misaelpaulmamangun@gmail.com>
2022-10-06 12:48:40 +0800

    refractor spacing and spelling

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 document.addEventListener('alpine:init', () => {
   Alpine.data('main', () => ({
-      features: [
-        { title: 'Mission', icon: 'fa-flag-checkered' },
-        { title: 'Strategy', icon: 'fa-industry' },
-        { title: 'Design', icon: 'fa-pencil' },
-        { title: 'Development', icon: 'fa-code' },
-        { title: 'Finance', icon: 'fa-money' },
-        { title: 'Consulting', icon: 'fa-headphones' }
-      ],
-      cards: [
-        { title: 'Ranking', number: 16},
-        { title: 'Client', number: 120},
-        { title: 'Cases', number: 977},
-        { title: 'Awards', number: 40}
-      ]
+    features: [
+      { title: 'Mission', icon: 'fa-flag-checkered' },
+      { title: 'Strategy', icon: 'fa-industry' },
+      { title: 'Design', icon: 'fa-pencil' },
+      { title: 'Development', icon: 'fa-code' },
+      { title: 'Finance', icon: 'fa-money' },
+      { title: 'Consulting', icon: 'fa-headphones' },
+    ],
+    cards: [
+      { title: 'Ranking', number: 16 },
+      { title: 'Client', number: 120 },
+      { title: 'Cases', number: 977 },
+      { title: 'Awards', number: 40 },
+    ],
   }))
 })
